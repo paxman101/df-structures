230f219939c623c6a1c56a250f6b4c2213332c7d
Paxton Schweigert <paxmanrocks@gmail.com>
2022-12-24 20:11:12 -0800

    Copy over initial xml parsing behavior from codegen.pl

--- codegen.py
+++ codegen.py
@@ -1,4 +1,5 @@
 import argparse
+
 from lxml import etree
 from pathlib import Path
 
@@ -15,18 +16,43 @@ def get_arguments() -> tuple[Path, Path, str]:
     return Path(args.input_directory), Path(args.output_directory), args.main_namespace
 
 
+# TODO: Move to a class
 def read_xml_files(input_directory: Path):
     type_dict = dict()
+    type_files_dict = dict()
     global_dict = dict()
+    global_files_dict = dict()
 
-    def check_bad_attrs(element):
-        pass
+    # lxml's Element type is unfortunately _Element isn't actually a "private" type
+    def check_bad_attrs(element: etree._Element, allow_size=False, allow_align=False):
+        if not allow_size and element.get("size"):
+            raise Exception(f"Cannot use size for {element}")
+        if element.get("offset"):
+            raise Exception(f"Cannot use offset for {element}")
+        if not allow_align and element.get("alignment"):
+            raise Exception(f"Cannot use alignment for {element}")
 
-    def add_type_to_dict(element):
-        pass
+    def add_type_to_dict(element: etree._Element, source_xml_path: Path):
+        type_name = element.get("type-name")
+        if not type_name:
+            raise Exception(f"type-name not defined for {element} in file {source_xml_path}")
 
-    def add_global_to_dict(element):
-        pass
+        if type_name in type_dict:
+            raise Exception(f"Duplicate definition of global {type_name}")
+        check_bad_attrs(element)
+        type_dict[type_name] = element
+        type_files_dict[type_name] = source_xml_path
+
+    def add_global_to_dict(element: etree._Element, source_xml_path: Path):
+        name = element.get("name")
+        if not name:
+            raise Exception(f"Global {element} without a name in file {source_xml_path}")
+
+        if name in global_dict:
+            raise Exception(f"Duplicate definition of global {name}")
+        check_bad_attrs(element)
+        global_dict[name] = element
+        global_files_dict[name] = source_xml_path
 
     # Import the two XSLT files in the script dir to transform the XML files we read.
     # Each XML file gets transformed by lower-1.xslt, then the result is then transformed by lower-2.xslt.
@@ -41,14 +67,17 @@ def read_xml_files(input_directory: Path):
         xml_etree = xslt2_transform(xslt1_transform(xml_etree))
         xml_doc_list.append(xml_etree)
         for global_type in xml_etree.iterfind(f"{ns_prefix}global-type"):
-            add_type_to_dict(global_type)
+            add_type_to_dict(global_type, xml_path)
         for global_object in xml_etree.iterfind(f"{ns_prefix}global-object"):
-            add_global_to_dict(global_object)
+            add_global_to_dict(global_object, xml_path)
+
+    return type_dict, type_files_dict, global_dict, global_files_dict
 
 
 def main():
     input_directory, output_directory, main_namespace = get_arguments()
-    read_xml_files(input_directory)
+    type_dict, type_files_dict, global_dict, global_files_dict = read_xml_files(input_directory)
+
 
 
 if __name__ == "__main__":
